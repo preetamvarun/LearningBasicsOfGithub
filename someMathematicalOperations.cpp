cb930904ed8d76888a554669f9bedfb1d02b2222
Preetam Varun <preetamarun99@gmail.com>
2021-07-17 11:16:12 +0530

    works on unsigned long long integers as well

--- someMathematicalOperations.cpp
+++ someMathematicalOperations.cpp
@@ -1,10 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
+typedef unsigned long long int ull;
 
 //Input range varies from [0,inf)
 
 
-bool checkEven(int x){
+bool checkEven(ull x){
 	bool answer = false;
 	if(x % 2 == 0){
 		answer = true;
@@ -14,7 +15,7 @@ bool checkEven(int x){
 
 
 int main(){
-	int n; cin>>n;
+	ull n; cin>>n;
 	checkEven(n) == true ? cout<<"It is a even number" : cout<<"it is odd number";
 	return 0;
 }
