01ec7674b4b82860e3d771cde1b57328429ff336
Preetam Varun <preetamarun99@gmail.com>
2021-07-17 11:14:00 +0530

    program works well on non negative integers to some range

---
+++ someMathematicalOperations.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Input range varies from [0,inf)
+
+
+bool checkEven(int x){
+	bool answer = false;
+	if(x % 2 == 0){
+		answer = true;
+	}
+	return answer;
+}
+
+
+int main(){
+	int n; cin>>n;
+	bool x = checkEven(n);
+	x == true ? cout<<"It is a even number" : cout<<"it is odd number";
+	return 0;
+}
